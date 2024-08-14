8bbbf1cfe9014f634f78a7a939cbcff5a62a907c
julietaaaaaaaaaaaa <julia.souza3@estudante.ifms.edu.br>
2024-08-14 16:09:13 -0400

    projetinho rs

---
+++ src/Home/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-reesponsive-carousel/lib/styles/carousel.min.css';
+
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        <p>Minha Home</p>
+        </>
+    );
+}
